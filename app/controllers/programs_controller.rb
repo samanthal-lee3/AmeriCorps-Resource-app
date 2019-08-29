835dd60c201e07b82e987dcf20a8136e86fa93ce
Samantha Lee <samanthal.lee3@gmail.com>
2019-08-29 14:07:21 +0000

    Setting up rspec to test index action

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -2,6 +2,9 @@ class ProgramsController < ApplicationController
 
     before_action :authenticate_user!, only: [:new, :create]
 
+def index
+end
+
 def new
 end
 
--- spec/controllers/programs_controller_spec.rb
+++ spec/controllers/programs_controller_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe ProgramsController, type: :controller do
 
+    describe "programs#index action" do
+        it "should successfully show all programs" do
+        #arrange
+        #program1 = FactoryBot.create(:program)
+
+        #act
+        #get :index
+
+        #assert
+        #expect(response).to have_http_status(:success)
+        end
+    end
 end
