26a376ba14497deff1569470d806c8a71b3eed80
Samantha Lee <samanthal.lee3@gmail.com>
2019-08-29 14:53:48 +0000

    Built route, view, and nav bar link to about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
+  get 'about', to: 'static_pages#about'
   resources :programs, :resources, only: [:new, :create, :index]
 end
