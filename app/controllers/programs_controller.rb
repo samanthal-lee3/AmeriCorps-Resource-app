c2c9878c6584db050bdc3e98daf5dd42e74d19a8
Samantha Lee <samanthal.lee3@gmail.com>
2019-08-29 13:07:37 +0000

    Generated controllers and defined basic routes for resources and programs

---
+++ app/controllers/programs_controller.rb
@@ -0,0 +1,12 @@
+class ProgramsController < ApplicationController
+
+def new
+end
+
+def create
+end
+
+def show
+end
+
+end
--- app/models/program.rb
+++ app/models/program.rb
@@ -1,2 +1,4 @@
 class Program < ApplicationRecord
+    belongs_to :user
+
 end
